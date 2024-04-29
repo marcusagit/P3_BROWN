66162e25ab9f3e076c8ef36efb4285b3e3d7d023
Marcus Allen <marcusacontact@gmail.com>
2024-04-29 13:29:32 +1000

    added other motors

--- main.c
+++ main.c
@@ -28,14 +28,37 @@ int main() {
 
     mpu6050_init();
 
+    // TODO: motor layout:
+    
     // setup stepper pins, sensor pins
     Stepper motor_a = stepper_default();
-    motor_a.gpio_a_pwm = 13;
-    motor_a.gpio_a_dir = 12;
-    motor_a.gpio_b_pwm = 11;
-    motor_a.gpio_b_dir = 10;
+    motor_a.gpio_a_pwm = 9;
+    motor_a.gpio_a_dir = 8;
+    motor_a.gpio_b_pwm = 7;
+    motor_a.gpio_b_dir = 6;
+    
+    Stepper motor_b = stepper_default();
+    motor_b.gpio_a_pwm = 13;
+    motor_b.gpio_a_dir = 12;
+    motor_b.gpio_b_pwm = 11;
+    motor_b.gpio_b_dir = 10;
+
+    Stepper motor_c = stepper_default();
+    motor_c.gpio_a_pwm = 18;
+    motor_c.gpio_a_dir = 19;
+    motor_c.gpio_b_pwm = 20;
+    motor_c.gpio_b_dir = 21;
+
+    Stepper motor_d = stepper_default();
+    motor_d.gpio_a_pwm = 14;
+    motor_d.gpio_a_dir = 15;
+    motor_d.gpio_b_pwm = 17;
+    motor_d.gpio_b_dir = 16;
 
     stepper_init(&motor_a);
+    stepper_init(&motor_b);
+    stepper_init(&motor_c);
+    stepper_init(&motor_d);
 
 
 
@@ -44,7 +67,11 @@ int main() {
         mpu6050_print_raw_values();
         stepper_rotate(&motor_a, 360);
         sleep_ms(500);
-        stepper_rotate(&motor_a, -360);
+        stepper_rotate(&motor_b, 360);
+        sleep_ms(500);
+        stepper_rotate(&motor_c, 360);
+        sleep_ms(500);
+        stepper_rotate(&motor_d, 360);
         sleep_ms(500);
     }
 }
