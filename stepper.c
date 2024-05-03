3c24d36cec7af028dbb777f4af81f16a9d4b984a
Marcus Allen <marcusacontact@gmail.com>
2024-05-03 23:50:43 +1000

    changed pwm pins to sio, and added stop after rotate

--- stepper.c
+++ stepper.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "hardware/pwm.h"
+// #include "hardware/pwm.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "pico/bootrom.h"
@@ -45,16 +45,18 @@ Stepper stepper_default(){
     return s;
 }
 
+
 void stepper_stop(Stepper *s){
-    s->current_step = 0;
-    gpio_put(s->gpio_a_dir, 0);
-    gpio_put(s->gpio_b_dir, 0);
-    pwm_set_gpio_level(s->gpio_a_pwm, 0);
-    pwm_set_gpio_level(s->gpio_b_pwm, 0);
+    gpio_put(s->gpio_a_pwm, 0);
+    gpio_put(s->gpio_b_pwm, 0);
 }
 
 // TODO: control delay/step speed (1ms is the minimum delay) 
-void stepper_step(Stepper *s, int dir){
+
+void stepper_step_no_delay(Stepper *s, int dir){
+    gpio_put(s->gpio_a_pwm, 1);
+    gpio_put(s->gpio_b_pwm, 1);
+
     if(dir > 0){
         s->current_step += 1;
         s->current_step = s->current_step % 4;
@@ -66,6 +68,10 @@ void stepper_step(Stepper *s, int dir){
         gpio_put(s->gpio_a_dir, a_step[s->current_step]);
         gpio_put(s->gpio_b_dir, b_step[s->current_step]);
     }
+}
+
+void stepper_step(Stepper *s, int dir){
+    stepper_step_no_delay(s, dir);
     sleep_ms(1);
 }
 
@@ -79,32 +85,26 @@ void stepper_rotate(Stepper *s, int deg){
     for(size_t i = 0; i < usteps; i++){
         stepper_step(s, steps);
     }
+    // NOTE: stops the buzz....
+    gpio_put(s->gpio_a_pwm, 0);
+    gpio_put(s->gpio_b_pwm, 0);
 }
 
 void stepper_init(Stepper *s){
-    // setup pwm 
-    gpio_set_function(s->gpio_a_pwm , GPIO_FUNC_PWM);
-    gpio_set_function(s->gpio_b_pwm, GPIO_FUNC_PWM);
+    gpio_init(s->gpio_a_pwm);
+    gpio_set_dir(s->gpio_a_pwm, GPIO_OUT);
+    gpio_put(s->gpio_a_pwm, 0);
 
-    uint slice_a = pwm_gpio_to_slice_num(s->gpio_a_pwm);
-    uint slice_b = pwm_gpio_to_slice_num(s->gpio_b_pwm);
-    uint chan_a = pwm_gpio_to_channel(s->gpio_a_pwm);
-    uint chan_b = pwm_gpio_to_channel(s->gpio_b_pwm);
-
-    pwm_set_wrap(slice_a, 3);
-    pwm_set_wrap(slice_b, 3);
-    pwm_set_chan_level(slice_a, chan_a, 3);
-    pwm_set_chan_level(slice_b, chan_b, 3);
-
-    pwm_set_enabled(slice_a, true);
-    pwm_set_enabled(slice_b, true);
+    gpio_init(s->gpio_b_pwm);
+    gpio_set_dir(s->gpio_b_pwm, GPIO_OUT);
+    gpio_put(s->gpio_b_pwm, 0);
 
     gpio_init(s->gpio_a_dir);
     gpio_set_dir(s->gpio_a_dir, GPIO_OUT);
-    // gpio_put(s->gpio_a_dir, 1);
+    gpio_put(s->gpio_a_dir, 0);
 
     gpio_init(s->gpio_b_dir);
     gpio_set_dir(s->gpio_b_dir, GPIO_OUT);
-    // gpio_put(s->gpio_b_dir, 1);
+    gpio_put(s->gpio_b_dir, 0);
 
 }
